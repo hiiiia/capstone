976701fcc7bd8dbd24ee8b9797e0349d6a574119
hiiiia <90683622+hiiiia@users.noreply.github.com>
2023-12-05 00:30:29 +0900

    d
    d

--- cap_app/App.tsx
+++ cap_app/App.tsx
@@ -21,9 +21,10 @@ import {
   Image,
   TextInput,
   TouchableHighlight,
-  Alert 
+  Alert
 } from 'react-native';
-import { Camera, CameraPermissionStatus, CameraDevice, PhotoFile, useCameraDevice } from 'react-native-vision-camera';
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+import { Camera, CameraPermissionStatus, CameraDevice, PhotoFile, useCameraDevice, VideoFile } from 'react-native-vision-camera';
 
 const App = () => {
   const device = useCameraDevice('back');
@@ -48,6 +49,28 @@ const App = () => {
     getPermission();
   }, []);
 
+  // 비디오 녹화
+  const recodevideo = async () => {
+    if (camera.current) {
+      try {
+        camera.current.startRecording({
+          //videoCodec: 'h265',
+          //videoBitRate: 'high',
+          onRecordingFinished: (video) => { sendVideoToServer(video) },
+          onRecordingError: (error) => console.error(error),
+        });
+      } catch (error) {
+        console.error("Error starting video recording:", error);
+      }
+    }
+  };
+
+  const stop_recodevideo = async () => {
+    if (camera.current) {
+      const video = await camera.current.stopRecording();
+    }
+  };
+
   // 사진 캡처 함수
   const capturePhoto = async () => {
     if (camera.current) {
@@ -58,12 +81,12 @@ const App = () => {
       });
 
       // 이미지를 서버로 전송
-      
+
       sendImageToServer(photo);
     }
   };
 
-    // 사진 predict 함수
+  // 사진 predict 함수
   const capturePhoto_to_predict = async () => {
     if (camera.current) {
       const photo = await camera.current.takePhoto({
@@ -73,28 +96,74 @@ const App = () => {
       });
 
       // 이미지를 서버로 전송
-      
+
       sendImageTopredict(photo);
     }
   };
   function getCurrentTimeFormatted() {
     const currentDate = new Date();
-  
+
     const year = currentDate.getFullYear().toString().slice(-2); // 년도의 끝 두 자리 (YY)
     const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 월 (MM), 0을 1월로 처리
     const day = String(currentDate.getDate()).padStart(2, '0'); // 일 (DD)
     const hours = String(currentDate.getHours()).padStart(2, '0'); // 시간 (HH)
     const minutes = String(currentDate.getMinutes()).padStart(2, '0'); // 분 (MM)
     const seconds = String(currentDate.getSeconds()).padStart(2, '0'); // 초 (SS)
-  
+
     return `${year}_${month}_${day}_${hours}_${minutes}_${seconds}`;
   }
 
 
+    // 비디오 서버로 전송하는 함수
+    const sendVideoToServer = async (video: VideoFile) => {
+      const path = video.path;
+    
+      try {
+        //console.log(`${path}`);
+        // Save the video file using CameraRoll
+        await CameraRoll.save(`file://${path}`, {
+          type: 'video',
+        });
+    
+        const formData = new FormData();
+    
+        // Append the video file to the formData
+        formData.append('video', {
+          uri: `file://${path}`,
+          type: 'video/mp4',  // Adjust the type based on your video file format
+          name: `${getCurrentTimeFormatted()}.mp4`,  // Adjust the extension based on your video file format
+        });
+    
+        const response = await fetch(`http://${server_address}/upload_video`, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+    
+        set_connect_state("NNNNN");
+    
+        if (response.ok) {
+          // Video successfully sent to the server
+          console.log("Send");
+          set_connect_state("send");
+        } else {
+          // Error occurred during the transmission
+          console.log("Send Error");
+          set_connect_state("Send Error");
+        }
+      } catch (error) {
+        console.log(error);
+        set_connect_state(`${error}`);
+        // Network error or other exceptions
+      }
+    };
+
   // 이미지를 서버로 전송하는 함수
   const sendImageToServer = async (image: PhotoFile) => {
     const formData = new FormData();
-    
+
     formData.append('image', {
       uri: `file://${image.path}`,
       type: 'image/jpeg',
@@ -131,7 +200,7 @@ const App = () => {
 
   const sendImageTopredict = async (image: PhotoFile) => {
     const formData = new FormData();
-    
+
     formData.append('image', {
       uri: `file://${image.path}`,
       type: 'image/jpeg',
@@ -165,7 +234,7 @@ const App = () => {
       set_connect_state(`${error}`);
       // 네트워크 오류 등으로 인한 예외 처리
     }
-};
+  };
 
 
   const handleLogin = () => {
@@ -173,7 +242,7 @@ const App = () => {
       user_id: username,
       password: password,
     };
-  
+
     fetch(`http://${server_address}/login`, {
       method: 'POST',
       headers: {
@@ -198,13 +267,13 @@ const App = () => {
         Alert.alert('네트워크 오류:', error);
       });
   };
-  
+
   const handleSignup = () => {
     const userData = {
       user_id: username,
       password: password,
     };
-  
+
     fetch(`http://${server_address}/signup`, {
       method: 'POST',
       headers: {
@@ -231,57 +300,60 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-    {!login_state ? (
-      // 로그인 창
-      <View style={styles.loginContainer}>
-  <TextInput
-    style={styles.login_input}
-    placeholder="Username"
-    value={username}
-    onChangeText={(text) => setUsername(text)}
-  />
-  <TextInput
-    style={styles.login_input}
-    placeholder="Password"
-    secureTextEntry
-    value={password}
-    onChangeText={(text) => setPassword(text)}
-  />
-  <TouchableHighlight
-    style={styles.login_summit_button}
-    onPress={handleLogin}
-  >
-    <Text style={styles.login_summit_button_text}>Login</Text>
-  </TouchableHighlight>
-  <TouchableHighlight
-    style={styles.signup_button}
-    onPress={handleSignup}
-  >
-    <Text style={styles.signup_button_text}>Sign Up</Text>
-  </TouchableHighlight>
-</View>
-    ) : (
-      // 카메라 및 기타 UI 요소
-      <>
-        <Camera
-          ref={camera}
-          style={styles.absoluteFill}
-          device={device as CameraDevice}
-          isActive={true}
-          photo={true}
-        />
-        <Text style={styles.sectionTitle}>state: {camera_state}</Text>
-        <Button title="Capture Photo" onPress={capturePhoto} />
-        <Button title="Capture Photo predict" onPress={capturePhoto_to_predict} />
-        <Text>Server connect : {`${connneting_state}`} Login ID : {`${username}`}</Text>
-        {capturedPhoto && (
-          <View>
-            <Image source={{ uri: `file://${capturedPhoto.path}` }} style={styles.capturedImage} />
-          </View>
-        )}
-      </>
-    )}
-  </View>
+      {!login_state ? (
+        // 로그인 창
+        <View style={styles.loginContainer}>
+          <TextInput
+            style={styles.login_input}
+            placeholder="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+          />
+          <TextInput
+            style={styles.login_input}
+            placeholder="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <TouchableHighlight
+            style={styles.login_summit_button}
+            onPress={handleLogin}
+          >
+            <Text style={styles.login_summit_button_text}>Login</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.signup_button}
+            onPress={handleSignup}
+          >
+            <Text style={styles.signup_button_text}>Sign Up</Text>
+          </TouchableHighlight>
+        </View>
+      ) : (
+        // 카메라 및 기타 UI 요소
+        <>
+          <Camera
+            ref={camera}
+            style={styles.absoluteFill}
+            device={device as CameraDevice}
+            isActive={true}
+            video={true}
+            // photo={true}
+          />
+          <Text style={styles.sectionTitle}>state: {camera_state}</Text>
+          <Button title="Recode Video" onPress={recodevideo} />
+          <Button title="Stop Recode Video" onPress={stop_recodevideo} />
+          <Button title="Capture Photo" onPress={capturePhoto} />
+          <Button title="Capture Photo predict" onPress={capturePhoto_to_predict} />
+          <Text>Server connect : {`${connneting_state}`} Login ID : {`${username}`}</Text>
+          {capturedPhoto && (
+            <View>
+              <Image source={{ uri: `file://${capturedPhoto.path}` }} style={styles.capturedImage} />
+            </View>
+          )}
+        </>
+      )}
+    </View>
   );
 };
 
@@ -312,7 +384,7 @@ const styles = StyleSheet.create({
   },
 
   loginContainer: {
-    zIndex : 3,
+    zIndex: 3,
     marginTop: 20,
     paddingHorizontal: 20,
   },
--- python_server/server.py
+++ python_server/server.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify
 import os
 import mysql.connector
 import subprocess
+import re
 from tensorflow import keras
 from keras.applications.inception_v3 import preprocess_input
 from PIL import Image
 import numpy as np
-
+import cv2
 
 global user_id_pk
 
@@ -273,10 +274,19 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# 비디오 저장 디렉토리
 
+UPLOAD_VIDEO_FOLDER = 'uploads_video'
+if not os.path.exists(UPLOAD_VIDEO_FOLDER):
+    os.makedirs(UPLOAD_VIDEO_FOLDER)
+    
+app.config['UPLOAD_VIDEO_FOLDER'] = UPLOAD_VIDEO_FOLDER
+
+
+# 예측데이터 저장 디렉토리
 PREDICT_FOLDER = 'predict'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+if not os.path.exists(PREDICT_FOLDER):
+    os.makedirs(PREDICT_FOLDER)
 
 app.config['PREDICT_FOLDER'] = PREDICT_FOLDER
 
@@ -310,6 +320,170 @@ def upload_image():
         map_id_insert(str(user_id_pk),str(map_id))
 
         return jsonify({'message': 'Image uploaded and saved as ' + filename})
+
+@app.route('/upload_video', methods=['POST'])
+def upload_video():
+    user_id_pk = "root"
+    print(user_id_pk)
+    
+    if 'video' not in request.files:
+        return jsonify({'error': 'No video part'})
+
+    video = request.files['video']
+    video_name = ""
+    if video.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if video:
+        video_name = video.filename
+        if not os.path.exists(app.config['UPLOAD_VIDEO_FOLDER']+'\\'+user_id_pk):
+            os.makedirs(app.config['UPLOAD_VIDEO_FOLDER']+'\\'+user_id_pk)
+            print("Making",user_id_pk)
+        print("None Making",user_id_pk)
+
+        filename = os.path.join(app.config['UPLOAD_VIDEO_FOLDER'], user_id_pk, video.filename)
+        video.save(filename)
+
+        if not os.path.exists(app.config['UPLOAD_VIDEO_FOLDER']+'\\'+user_id_pk+'\\'+"frames"):
+            os.makedirs(app.config['UPLOAD_VIDEO_FOLDER']+'\\'+user_id_pk+'\\'+"frames")
+        output_dir = os.path.join(app.config['UPLOAD_VIDEO_FOLDER'], user_id_pk,"frames")
+
+        # Add your video processing logic here, if needed
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # 비디오 파일 열기
+        video = cv2.VideoCapture(filename)
+
+        # 비디오가 열렸는지 확인
+        if not video.isOpened():
+            print("Error: Could not open video.")
+            exit()
+
+        target_width = 752
+        target_height = 480
+
+        # 리사이즈할 크기 설정
+        resize_dim = (target_width, target_height)
+
+        start_timestamp_ns = 10000000000  # 시작 타임스탬프 (나노초 단위)
+        frame_interval_ns = 33333333    # 30 FPS에 해당하는 프레임 간격 (나노초 단위)
+        count = 0
+        timestamps = []  # 타임스탬프를 저장할 리스트
+
+        while True:
+            # 비디오에서 프레임 읽기
+            ret, frame = video.read()
+
+            # 프레임이 없으면 종료
+            if not ret:
+                break
+
+            # 흑백으로 변환
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            # 이미지 리사이징
+            resized_frame = cv2.resize(gray_frame, resize_dim, interpolation = cv2.INTER_AREA)
+
+            # 현재 프레임의 타임스탬프 계산
+            current_timestamp_ns = start_timestamp_ns + (count * frame_interval_ns)
+            timestamps.append(current_timestamp_ns)
+
+            # 타임스탬프를 파일 이름으로 사용하여 프레임 저장
+            frame_filename = os.path.join(output_dir, "{}.png".format(current_timestamp_ns))
+            cv2.imwrite(frame_filename, resized_frame)  # 리사이징된 흑백 프레임 저장
+
+            # 프레임 번호 출력
+            print(count)
+
+            count += 1
+
+        # 비디오 파일 닫기
+        video.release()
+
+        # 타임스탬프를 별도의 파일에 저장
+        timestamps_file = os.path.join(output_dir, 'timestamps.txt')
+        with open(timestamps_file, 'w') as file:
+            for timestamp in timestamps:
+                file.write("{}\n".format(timestamp))
+
+        print("Grayscale frames and timestamps have been saved in the folder:", timestamps_file)
+        
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        
+        # Command execution
+        command = [
+            "./Examples/Monocular/mono_euroc",
+            "./Vocabulary/ORBvoc.txt",
+            "./Examples/Monocular/EuRoC.yaml",
+            # "/home/hello/Desktop/data/frames",
+            str(output_dir),
+            #"/home/hello/Desktop/data/frames/timestamps.txt",
+            str(output_dir+"\\timestamps.txt"),
+            #"test"
+            str(video_name)
+            
+        ]
+
+        # 결과를 저장할 파일
+        output_file = "out_result.txt"
+
+        # subprocess.run을 사용하여 명령을 foreground에서 실행하고 결과를 파일로 저장합니다.
+        try:
+            with open(output_file, "w") as output_file_handle:
+                result = subprocess.run(command, check=True, stdout=output_file_handle, stderr=subprocess.PIPE, text=True)
+                print("프로세스 종료 코드:", result.returncode)
+        except subprocess.CalledProcessError as e:
+            print("오류 발생. 종료 코드:", e.returncode)
+            print("표준 에러:\n", e.stderr)
+
+
+        x_position = 0
+        y_position = 0
+        z_position = 0
+        # Open the file for reading
+        with open(output_file, "r") as output_file_handle:
+            # Read the entire contents of the file
+            file_contents = output_file_handle.read()
+
+            # Use regular expressions to extract values
+            match = re.search(r"Position: x = (\S+), y = (\S+), z = (\S+)", file_contents)
+
+            # Check if the pattern was found
+            if match:
+                x_value = match.group(1)
+                y_value = match.group(2)
+                z_value = match.group(3)
+
+                x_position = x_value
+                y_position = y_value
+                z_position = z_value
+                # Print or use the extracted values
+                print(f"Extracted values: x = {x_value}, y = {y_value}, z = {z_value}")
+            else:
+                print("Pattern not found in the file.")
+
+
+
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        # =====================================================
+        map_id = video_name
+        map_id_insert(str(user_id_pk), str(map_id))
+
+        return jsonify({'message': 'Video uploaded and saved as ' + filename})
 
 
 @app.route('/predict', methods=['POST'])
