4db4f2ad091231f7f8006d4371712c12a0ea08c0
catlover6 <112870380+catlover6@users.noreply.github.com>
2023-12-09 04:19:10 -0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -22,10 +22,12 @@ import {
   TextInput,
   TouchableHighlight,
   Alert,
-  Modal
+  Modal,
+  FlatList, TouchableOpacity
 } from 'react-native';
 import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 import { Camera, CameraPermissionStatus, CameraDevice, PhotoFile, useCameraDevice, VideoFile } from 'react-native-vision-camera';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const device = useCameraDevice('back');
@@ -34,14 +36,23 @@ const App = () => {
   const [camera_state, set_camera_state] = useState('');
   const [capturedPhoto, setCapturedPhoto] = useState<PhotoFile | null>(null);
 
-
+  const [user_map_id, set_user_map] = useState('None');
   const [login_state, set_login_state] = useState(false);
   const [username, setUsername] = useState(''); // 입력받은 사용자명
   const [password, setPassword] = useState(''); // 입력받은 비밀번호
 
-  const [server_address, set_server_address] = useState('192.168.4.70:5000');
+  const [server_address, set_server_address] = useState('192.168.0.248:5000');
   const [inputServerAddress, setInputServerAddress] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const [map_modal_visible, set_map_modal] = useState(false);
+  const [modalData, setModalData] = useState<Array<Array<string | number>>>([]);
+  const [selectedItem, setSelectedItem] = useState(0 as number);
+
+  const [loop_id,set_loop_id] = useState<null | ReturnType<typeof setTimeout>>(null);
+  const [navivation, set_naviation] = useState("")
+  
+  
   
   const handleUpdateServerAddress = () => {
     set_server_address(inputServerAddress);
@@ -91,6 +102,22 @@ const App = () => {
       };
   
       
+        // 비디오 녹화
+  const find_current_pos = async () => {
+    if (camera.current) {
+      try {
+        camera.current.startRecording({
+          //videoCodec: 'h265',
+          //videoBitRate: 'high',
+          onRecordingFinished: (video) => { find_path_send_VideoToServer(video) },
+          onRecordingError: (error) => console.error(error),
+        });
+      } catch (error) {
+        console.error("Error starting video recording:", error);
+      }
+    }
+  };
+
   const stop_recodevideo = async () => {
     if (camera.current) {
       const video = await camera.current.stopRecording();
@@ -143,6 +170,76 @@ const App = () => {
   }
 
 
+    const get_user_map_compo = ()=> {
+      
+    const userData = {
+      user_map: user_map_id,
+    };
+
+    fetch(`http://${server_address}/get_data_by_map_id`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // 성공적으로 데이터를 받아왔을 때의 처리
+        const twoDArray = data.data;
+        //console.log('Received data:', twoDArray);
+        setModalData(twoDArray);
+        set_map_modal(true);
+        //console.log('Received data:', data.data);
+      } else {
+        // 데이터를 받아오지 못했을 때의 처리
+        console.error('Failed to fetch data:', data.data);
+      }
+    })
+    .catch(error => {
+      // 네트워크 오류 등의 예외 처리
+      console.error('Error fetching data:', error);
+    });
+
+
+  };
+
+  const handleItemPress = (index : number) => {
+
+    setSelectedItem(index+1);
+  };
+  
+  const handleItemSubmit = () => {
+  
+    set_map_modal(false);
+    
+    // Execute find_object every 3 seconds
+    const intervalId = setInterval(() => {
+      find_object();
+    }, 10000);
+
+    set_loop_id(intervalId);
+    // Stop the interval after 12 seconds(12,000) (3 executions)
+    setTimeout(() => {
+      clearInterval(intervalId);
+    }, 120000); // 120000 -> 120 second
+  };
+
+  const handel_stop_loop = () =>{
+    clearInterval(loop_id as ReturnType<typeof setTimeout>);
+    set_loop_id(null);
+    set_connect_state("Stop Finding");
+  }
+  
+  const find_object = () => {
+    find_current_pos();
+    setTimeout(() => {
+      stop_recodevideo();
+    }, 2000);
+  };
+
+  
     // 비디오 서버로 전송하는 함수
     const sendVideoToServer = async (video: VideoFile) => {
       const path = video.path;
@@ -223,6 +320,7 @@ const App = () => {
               // Video successfully sent to the server
               console.log("Send");
               set_connect_state("send");
+              
             } else {
               // Error occurred during the transmission
               console.log("Send Error");
@@ -234,6 +332,59 @@ const App = () => {
             // Network error or other exceptions
           }
         };
+
+        const find_path_send_VideoToServer = async (video: VideoFile) => {
+          const path = video.path;
+        
+          try {
+            // Save the video file using CameraRoll
+            await CameraRoll.save(`file://${path}`, {
+              type: 'video',
+            });
+        
+            const formData = new FormData();
+        
+            // Append the video file to the formData
+            formData.append('video', {
+              uri: `file://${path}`,
+              type: 'video/mp4',
+              name: `${getCurrentTimeFormatted()}.mp4`,
+            });
+        
+            // Append the 'number' value to the formData
+            formData.append('number', selectedItem);
+        
+            const response = await fetch(`http://${server_address}/find_path`, {
+              method: 'POST',
+              body: formData,
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            });
+        
+            set_connect_state("NNNNN");
+        
+            if (response.ok) {
+              // Video and number successfully sent to the server
+              const result = await response.json(); // 수정된 부분
+              //console.log(result.message); // 서버에서 받은 데이터 출력
+              console.log(result.result); // 서버에서 받은 데이터 출력
+              
+              set_connect_state("Finding..");
+              set_naviation(result.result as string);
+            } else {
+              // Error occurred during the transmission
+              console.log("Send Error");
+              set_connect_state("Send Error");
+            }
+          } catch (error) {
+            console.log(error);
+            set_connect_state(`${error}`);
+            // Network error or other exceptions
+          }
+        };
+        
+
   // 이미지를 서버로 전송하는 함수
   const sendImageToServer = async (image: PhotoFile) => {
     const formData = new FormData();
@@ -330,6 +481,13 @@ const App = () => {
           // 로그인 성공 시, login_state를 true로 변경하여 화면을 전환
           Alert.alert('로그인 성공');
           set_login_state(true);
+          
+          if(data.map_id === "No_Maps"){
+            Alert.alert("Map을 생성하세요");
+          }
+          else{
+            set_user_map(data.map_id);
+          }
         } else {
           // 로그인 실패 처리 (예: 오류 메시지 표시)
           Alert.alert('로그인 실패', data.error);
@@ -397,13 +555,13 @@ const App = () => {
             style={styles.login_summit_button}
             onPress={handleLogin}
           >
-            <Text style={styles.login_summit_button_text}>Login</Text>
+            <Text style={styles.login_summit_button_text}>로그인</Text>
           </TouchableHighlight>
           <TouchableHighlight
             style={styles.signup_button}
             onPress={handleSignup}
           >
-            <Text style={styles.signup_button_text}>Sign Up</Text>
+            <Text style={styles.signup_button_text}>가입</Text>
           </TouchableHighlight>
           
 
@@ -413,7 +571,7 @@ const App = () => {
               onPress={() => setIsModalVisible(true)}
               disabled={login_state}
             >
-              <Text style={styles.updateServerButtonText}>Update Server</Text>
+              <Text style={styles.updateServerButtonText}>서버 주소 변경</Text>
             </TouchableHighlight>
           </View>
 
@@ -427,7 +585,7 @@ const App = () => {
             <View style={styles.modalContainer}>
               <TextInput
                 style={styles.modalInput}
-                placeholder="192.168.4.31:5000"
+                placeholder="192.168.0.248:5000"
                 placeholderTextColor="gray"
                 value={inputServerAddress}
                 onChangeText={(text) => setInputServerAddress(text)}
@@ -454,18 +612,75 @@ const App = () => {
             video={true}
             // photo={true}
           />
+         
+         <Modal
+  visible={map_modal_visible}
+  animationType="slide"
+  transparent={false}
+  onRequestClose={() => set_map_modal(false)}
+>
+  <View style={styles.modalContainer}>
+    {/* 모달의 헤더 등 추가 UI */}
+    <Text style={styles.modalHeaderText}>저장된 물체의 위치 & 종류</Text>
+    <TouchableOpacity style={styles.Modal_submit} onPress={() => handleItemSubmit()}>
+      <Text style = {{color : 'white'}}>선택</Text>
+    </TouchableOpacity>
+    {/* FlatList를 사용하여 데이터 리스트 표시 */}
+    <FlatList
+  data={modalData}
+  keyExtractor={(item, index) => index.toString()}
+  renderItem={({ item, index }) => (
+    <TouchableOpacity
+    style={[styles.itemContainer, { backgroundColor: selectedItem === index ? 'lightblue' : 'lightgray' }]}
+      onPress={() => handleItemPress(index)}
+    >
+      <Text>Number: {item[0]}</Text>
+      <Text>X: {item[1]}</Text>
+      <Text>Y: {item[2]}</Text>
+      <Text>Z: {item[3]}</Text>
+      <Text>Label: {item[4]}</Text>
+    </TouchableOpacity>
+  )}
+/>
+
+
+    {/* 모달 닫기 버튼 */}
+    <TouchableOpacity onPress={() => {set_map_modal(false)}}>
+      <Text style={styles.closeButton}>Close</Text>
+    </TouchableOpacity>
+  </View>
+</Modal>
+
           <Text style={styles.sectionTitle}>state: {camera_state}</Text>
-          <Button title="Recode Video" onPress={recodevideo} />
-          <Button title="Recode Predict Video" onPress={recode_predict_video} />
-          <Button title="Stop Recode Video" onPress={stop_recodevideo} color={'lightcoral'} />
+          <Button title="Map 생성" onPress={recodevideo} />
+          <Button title="물체 위치 저장" onPress={recode_predict_video} />
+          <Button title="촬영 정지" onPress={stop_recodevideo} color={'lightcoral'} />
           {/* <Button title="Capture Photo" onPress={capturePhoto} />
           <Button title="Capture Photo predict" onPress={capturePhoto_to_predict} /> */}
-          <Text>Server connect : {`${connneting_state}`} Login ID : {`${username}`}</Text>
+          <Button title={"생성된 Map : "+user_map_id} onPress={get_user_map_compo} disabled={user_map_id === "None"} color={'gray'} />
+          <Button title={"추적 중지"} onPress={handel_stop_loop} disabled={loop_id === null} color={'lightcoral'} />
+         <Text>서버상태 : {`${connneting_state} / ${navivation}`} / ID : {`${username}`}</Text>
           {capturedPhoto && (
             <View>
               <Image source={{ uri: `file://${capturedPhoto.path}` }} style={styles.capturedImage} />
+              
             </View>
           )}
+          {navivation === "East" ? (
+        <Image source={require('./assets/right.png')} style={styles.arrow} />
+      ) : (<></>)}
+
+{navivation === "West" ? (
+        <Image source={require('./assets/left.png')} style={styles.arrow} />
+      ) : (<></>)}
+
+{navivation === "North" ? (
+        <Image source={require('./assets/up.png')} style={styles.arrow} />
+      ) : (<></>)}
+
+{navivation === "South" ? (
+        <Image source={require('./assets/down.png')} style={styles.arrow} />
+      ) : (<></>)}
         </>
       )}
     </View>
@@ -552,7 +767,7 @@ const styles = StyleSheet.create({
     color : 'black',
 
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
     // Styles for the modal container
   },
@@ -565,6 +780,32 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
   },
+  modalHeaderText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  itemContainer: {
+    marginBottom: 20,
+    backgroundColor : 'lightgray'
+  },
+  closeButton: {
+    backgroundColor : 'lightcoral',
+    fontSize: 16,
+    color: 'white',
+    marginTop: 20,
+  },
+  Modal_submit : {
+    backgroundColor: 'blue' ,
+     color : 'white'
+  },
+  arrow : {
+    position :'relative',
+    top : '50%',
+    left : '50%',
+    width : 100,
+    height : 100
+  }
 });
 
 export default App;
